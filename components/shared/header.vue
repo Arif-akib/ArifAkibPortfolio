e31695b26c8d6a12cbf44d112a33963437ab88a3
Arif akib <a.rabbani.akib@gmail.com>
2024-09-16 17:53:27 +0600

    bug fix

--- components/shared/header.vue
+++ components/shared/header.vue
@@ -14,7 +14,7 @@
   </div>
   
   
-  <div @click="navbarDrop = false" class="text-2xl text-white font-bold bg-[#030303c2]  px-[5%] fixed top-0 h-[100svh] w-full backdrop-blur-sm z-50 duration-500" :class="navbarDrop == false ? 'translate-y-[-100%]' : 'translate-y-0' ">
+  <div @click="navbarDrop = false" class="text-2xl text-white font-bold bg-[#030303c2]  px-[5%] fixed top-0 h-[100vh] w-full backdrop-blur-sm z-50 duration-500" :class="navbarDrop == false ? 'translate-y-[-100%]' : 'translate-y-0' ">
     <div class="flex flex-col justify-center items-center gap-5 size-full -mt-10">
       <p class="absolute top-[5%] right-[5%] hover:rotate-90 duration-500"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="45" height="45" fill="currentColor"><path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20ZM12 10.5858L14.8284 7.75736L16.2426 9.17157L13.4142 12L16.2426 14.8284L14.8284 16.2426L12 13.4142L9.17157 16.2426L7.75736 14.8284L10.5858 12L7.75736 9.17157L9.17157 7.75736L12 10.5858Z"></path></svg></p>
       <a href="#Home" class="hover:text-gray-400 duration-500">Home</a>
